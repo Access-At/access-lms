f6e31980c0ec841453dbbcbf1b47492aab1e1b26
Rizsyad AR <rizsyad@digitama.id>
2024-08-03 18:00:47 +0700

    feat: add error not found page, and unauth

--- resources/frontend/routes/dashboard.tsx
+++ resources/frontend/routes/dashboard.tsx
@@ -3,16 +3,21 @@ import * as React from "react"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
 import Loading from "@/components/shared/loading"
+import NotFoundError from "@/pages/errors/NotFoundError"
 
 const DashboardLayout = React.lazy(
   () => import("@/components/layouts/dashboardLayout"),
 )
 
 export const Route = createFileRoute("/dashboard")({
-  beforeLoad: ({ context }) => {
+  notFoundComponent: () => <NotFoundError />,
+  beforeLoad: ({ context, location }) => {
     if (!context.auth.isAuthenticated) {
       throw redirect({
         to: "/auth",
+        search: {
+          redirect: location.href,
+        },
       })
     }
   },
