7d5f01718d1275c51314036db9476002521e9581
Rizsyad AR <rizsyad@digitama.id>
2024-08-03 22:07:00 +0700

    feat: add react-quil more frendly

--- resources/frontend/components/organisms/pagesContent.tsx
+++ resources/frontend/components/organisms/pagesContent.tsx
@@ -1,7 +1,32 @@
+import "react-quill/dist/quill.snow.css"
+
 import { LayoutComponents } from "@/components/atoms/layoutComponents"
+import { useState } from "react"
+import ReactQuill from "react-quill"
 import { UserNav } from "../molecules/userNav"
 
 export default function Pagescontent() {
+  const [value, setValue] = useState("")
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+      ["link", "image", "video", "formula"],
+
+      [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+      [{ script: "sub" }, { script: "super" }], // superscript/subscript
+      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+      //   [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ font: [] }],
+      [{ align: [] }],
+
+      ["clean"],
+    ],
+  }
+
   return (
     <LayoutComponents>
       <LayoutComponents.Header className='flex justify-between md:bg-white'>
@@ -12,7 +37,13 @@ export default function Pagescontent() {
       </LayoutComponents.Header>
       <LayoutComponents.Body className='flex min-h-[calc(100vh-5rem)] w-full flex-col items-center justify-center'>
         <div className='w-full max-w-3xl'>
-          {/* Disini nih biar di tengah */}
+          <ReactQuill
+            theme='snow'
+            value={value}
+            onChange={setValue}
+            modules={modules}
+            className='p-5'
+          />
         </div>
       </LayoutComponents.Body>
     </LayoutComponents>
